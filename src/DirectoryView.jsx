bba0f575f768e302223fd6776451ba74a9756125
vikas-cse123 <sagarraothakur2@gmail.com>
2026-03-14 10:39:30 +0530

    change My drive to home everywhere

--- src/DirectoryView.jsx
+++ src/DirectoryView.jsx
@@ -53,7 +53,7 @@ function DirectoryView() {
   const loadDirectory = async () => {
     try {
       const data = await getDirectoryItems(dirId);
-      setDirectoryName(dirId ? data.name : "My Drive");
+      setDirectoryName(dirId ? data.name : "Home");
       setDirectoriesList([...data.directories].reverse());
       setFilesList([...data.files].reverse());
     } catch (err) {
