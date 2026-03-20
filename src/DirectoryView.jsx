2f04f45131034150cda93cc59131365e48ee28ba
vikas-cse123 <sagarraothakur2@gmail.com>
2026-03-20 13:29:08 +0530

    Set original name of home dir

--- src/DirectoryView.jsx
+++ src/DirectoryView.jsx
@@ -53,7 +53,7 @@ function DirectoryView() {
   const loadDirectory = async () => {
     try {
       const data = await getDirectoryItems(dirId);
-      setDirectoryName(dirId ? data.name : "POKEMON");
+      setDirectoryName(dirId ? data.name : "CLOUDBOX");
       setDirectoriesList([...data.directories].reverse());
       setFilesList([...data.files].reverse());
     } catch (err) {
